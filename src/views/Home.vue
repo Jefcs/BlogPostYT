747f29e53c15841b37e1fcbfe470162b0d46a6bc
Jefcs <jeffersonc.silva@hotmail.com>
2021-08-06 15:56:10 -0300

    Changes to home and creating blogpost view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,40 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
+    <BlogPost :post="welcomeScreen" />
+    <BlogPost :post="post" v-for="(post, index) in sampleBlogPost" :key="index"/>
   </div>
 </template>
 
 <script>
+import BlogPost from '../components/BlogPost.vue';
+
 export default {
   name: "Home",
-  components: {},
+  components: {
+    BlogPost
+  },
+  data() {
+    return {
+      welcomeScreen: {
+        title: 'Welcome!',
+        blogPost: 
+        'Weekly blog articles with all thigs programmming including HTMl, CSS, JavaScript and more. Register todau to never miss a post!',
+        welcomeScreen: true,
+        photo: 'coding',
+      },
+      sampleBlogPost: [
+        {
+          title: 'This is a Filter Title!',
+          blogHTML: 'This is a filter blog post title!',
+          blogCoverPhoto: "beautiful-stories",
+        },
+        {
+          title: 'This is a Filter Title!',
+          blogHTML: 'This is a filter blog post title!',
+          blogCoverPhoto: "designed-for-everyone",
+        }
+      ]
+    }
+  }
 };
 </script>
