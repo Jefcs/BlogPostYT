affac66bff72d6440b3169a26b1a933146ce6245
Jefcs <jeffersonc.silva@hotmail.com>
2021-08-10 17:34:58 -0300

    Added cards components

--- src/components/BlogPost.vue
+++ src/components/BlogPost.vue
@@ -1,23 +1,31 @@
 <template>
   <div class="blog-wrapper no-user">
     <div class="blog-content">
-        <div>
-            <h2 v-if="post.welcomeScreen">{{post.title}}</h2>
-            <h2 v-else>{{post.title}}</h2>
-            <p v-if="post.welcomeScreen">{{post.blogPost}}</p>
-            <p class="content-preview" v-else>{{post.blogHTML}}</p>
-            <router-link class="link link-light" v-if="post.welcomeScreen" to="#">
-                Login/Register<Arrow class="arrow arrow-light" />
-            </router-link>
-            <router-link class="link" v-else to="#">
-                View The Post<Arrow class="arrow" />
-            </router-link>
-        </div>
+      <div>
+        <h2 v-if="post.welcomeScreen">{{ post.title }}</h2>
+        <h2 v-else>{{ post.title }}</h2>
+        <p v-if="post.welcomeScreen">{{ post.blogPost }}</p>
+        <p class="content-preview" v-else>{{ post.blogHTML }}</p>
+        <router-link class="link link-light" v-if="post.welcomeScreen" to="#">
+          Login/Register<Arrow class="arrow arrow-light" />
+        </router-link>
+        <router-link class="link" v-else to="#">
+          View The Post<Arrow class="arrow" />
+        </router-link>
+      </div>
     </div>
-        <div class="blog-photo">
-            <img v-if="post.welcomeScreen" :src="require(`../assets/blogPhotos/${post.photo}.jpg`)" alt="">
-            <img v-else :src="require(`../assets/blogPhotos/${post.blogCoverPhoto}.jpg`)" alt="">
-        </div>
+    <div class="blog-photo">
+      <img
+        v-if="post.welcomeScreen"
+        :src="require(`../assets/blogPhotos/${post.photo}.jpg`)"
+        alt=""
+      />
+      <img
+        v-else
+        :src="require(`../assets/blogPhotos/${post.blogCoverPhoto}.jpg`)"
+        alt=""
+      />
+    </div>
   </div>
 </template>
 
@@ -25,19 +33,20 @@
 import Arrow from '../assets/Icons/arrow-right-light.svg';
 
 export default {
-    name: 'blogPost',
-    props: ["post"],
-    components: {
-        Arrow
-    }
-}
+  name: 'blogPost',
+  props: ['post'],
+  components: {
+    Arrow
+  }
+};
 </script>
 
 <style lang="scss" scoped>
 .blog-wrapper {
   display: flex;
   flex-direction: column;
-  box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);
+  box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1),
+    0 2px 4px -1px rgba(0, 0, 0, 0.06);
   @media (min-width: 700px) {
     min-height: 650px;
     max-height: 650px;
@@ -105,7 +114,8 @@ export default {
   .blog-photo {
     order: 1;
     flex: 3;
-    box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);
+    box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1),
+      0 2px 4px -1px rgba(0, 0, 0, 0.06);
     @media (min-width: 700px) {
       order: 2;
     }
