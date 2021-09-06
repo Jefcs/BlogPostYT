f2b61b90c571549e2b97ed596e8679008293c960
Jefcs <jeffersonc.silva@hotmail.com>
2021-09-06 17:25:00 -0300

    Added method to remove a post

--- src/components/BlogCard.vue
+++ src/components/BlogCard.vue
@@ -4,7 +4,7 @@
       <div class="icon">
         <Edit class="edit" />
       </div>
-      <div class="icon">
+      <div @click="deletePost" class="icon">
         <Delete class="delete" />
       </div>
     </div>
@@ -42,6 +42,11 @@ export default {
   computed: {
     editPost() {
       return this.$store.state.editPost;
+    }
+  },
+  methods: {
+    deletePost() {
+      this.$store.dispatch('deletePost', this.post.blogID);
     }
   }
 };
